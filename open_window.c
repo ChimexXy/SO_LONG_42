dd39c1e1cb93fdaecb224c626a35588df23425df
chimexXy <moadzahnoun37@gmail.com>
2025-03-26 21:49:10 +0000

    chimex

--- open_window.c
+++ open_window.c
@@ -6,7 +6,7 @@
 /*   By: mozahnou <mozahnou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 01:16:51 by mozahnou          #+#    #+#             */
-/*   Updated: 2025/03/26 07:44:57 by mozahnou         ###   ########.fr       */
+/*   Updated: 2025/03/26 21:48:41 by mozahnou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 void	window_open(mlx_s *mlx, x_game *game)
 {
-		printf("%d\n",  game->len_line);
-	printf("%d\n",  game->wid_line);
 	mlx->mlx_init = mlx_init(); 
-	mlx->mlx_win = mlx_new_window(mlx->mlx_init, game->len_line * 32, game->wid_line * 32, "so_long");
+	mlx->mlx_win = mlx_new_window(mlx->mlx_init, game->len_line * 64, game->wid_line * 64, "so_long");
 	mlx_loop(mlx->mlx_init);
 }
